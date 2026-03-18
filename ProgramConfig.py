bc2c8981c6149c382d137641cadcdf8834f015ed
Hqndler <handler2104@gmail.com>
2026-03-18 15:36:49 +0100

    add start, stop , restart, status

--- ProgramConfig.py
+++ ProgramConfig.py
@@ -1,5 +1,15 @@
 import os, sys, time, subprocess, logging, yaml, readline, shlex
 from typing import List, Dict, Optional
+from enum import Enum
+
+class State(Enum):
+	INIT = "INITIALISATION"
+	STARTING = "STARTING"
+	RUNNING = "RUNNING"
+	STOPPED = "STOPPED"
+	FAILED = "FAILED"
+	KILLED = "KILLED"
+	SUCCESS = "SUCCESS"
 
 class ProgramConfig:
 	cmd = (str, "")
@@ -48,8 +58,11 @@ class TabComplete:
 class FdManager:
 
 	def __init__(self, stdout, stderr):
+		# /dev/null -> -3
 		self.stdout = open(stdout, 'a') if stdout != '/dev/null' else stdout
 		self.stderr = open(stderr, 'a') if stderr != '/dev/null' else stderr
+		# self.stdout = stdout
+		# self.stderr = stderr
 
 	def close(self):
 		for fd in (self.stdout, self.stderr):
@@ -69,22 +82,59 @@ class ProcessInstance:
 	"HERITE DE :"
 	"FD - ENVP - CWD - UID/GID"
 
-	def __init__(self):
-		self.pid
-		self.config: ProgramConfig
+	def __init__(self, config: ProgramConfig, index: int):
+		self.pid: int = 0
+		self.index = index
+		self.config: ProgramConfig = config
 		self.fds: FdManager = FdManager(self.config.stdout, self.config.stdout)
-		pass
+		self.state: State = State.STOPPED
 	
 	def start(self):
-		self.pid = subprocess.Popen(
-			args=shlex.split(self.config),
-			stdout=self.fds.stdout,
-			stderr=self.fds.stderr,
-			umask=self.config.umask,
-			env=self.config.env,
-			cwd=self.config.workingdir
-		)
-		pass
+		print(shlex.split(self.config.cmd))
+		print(self.config)
+		# env = os.environ
+		# env.update(self.config.env)
+		# print(env)
+		if not self.config.env:
+			self.config.env = {}
+
+		self.state = State.INIT
+
+		try:
+			self.pid = subprocess.Popen(
+				args=shlex.split(self.config.cmd),
+				stdout=self.fds.stdout,
+				stderr=self.fds.stderr,
+				umask=self.config.umask,
+				env= dict(os.environ, **self.config.env),
+				cwd=self.config.workingdir
+			)
+			self.state = State.STARTING
+		except OSError:
+			self.state = State.FAILED
+			return # ?
+
+		print(self.pid)
+
+	def stop(self):
+		if self.state != State.RUNNING:
+			return
+		
+		self.pid.terminate()
+		self.state = State.STOPPED
+		try:
+			self.pid.wait(timeout=self.config.stoptime)
+		except subprocess.TimeoutExpired:
+			self.pid.kill()
+			self.state = State.KILLED
+
+	def status(self):
+		if self.pid.poll() == None:
+			# TODO
+			print(f"{self.config.name}_{self.index} is RUNNING")
+			self.state = State.RUNNING  # à changer
+		else:
+			print(f"{self.config.name}_{self.index} is {self.state.value}")
 
 
 class Taskmaster:
@@ -95,12 +145,17 @@ class Taskmaster:
 		self.running: bool = True
 		TabComplete.key_words.extend(list(self.configs.keys()))
 		TabComplete.key_words.extend(["start", "stop", "restart", "reload"])
-		print(TabComplete.key_words)
+		# print(TabComplete.key_words)
 
 	def setup(self):
 		"lancer tout les process avec process.start()"
 		"lancer la main loop avec run shell"
 		"clean les process pour l'exit"
+		
+		for name, conf in self.configs.items():
+			if conf.autostart:
+				self.start(name)
+		
 		self.run_shell()
 		pass
 
@@ -111,25 +166,60 @@ class Taskmaster:
 	def status(self):
 		"afficher le status des process (running, existed, etc.)"
 		print("status GENERAL")
+		for k, v in self.instance.items():
+			for proc in v:
+				proc.status()
+		# if self.instance.Popen.poll() == None:
+			# print("instance X is RUNNING")
+			# return
+		# print("instance X is ", self.instance.state)
 
 	def reload(self):
 		"relancer le parsing du yml"
 		print("reload du yml")
 		pass
 
-	def start(self, parts):
+	def start(self, prog):
 		"démarrer si besoin le programme avec le bon nombre de process"
-		print("start de", parts)
+		print("start de", prog)
+
+		if not prog in self.configs:
+			return
+		
+		# if self.state == State.RUNNING:
+			# return
+
+		proc_instance = []
+
+		config: ProgramConfig = self.configs[prog]
+
+		for i in range(config.numprocs):
+			process = ProcessInstance(config, i)
+			process.start()
+			proc_instance.append(process)
+
+		self.instance[prog] = proc_instance
+
 		pass
 
-	def stop(self, parts):
+	def stop(self, prog):
 		"arrêter le programme s'il existe avec tout ses process"
-		print("stop de", parts)
+		print("stop de", prog)
+		if not prog in self.configs:
+			return
+		
+		print(self.instance)
+		
+		for process in self.instance[prog]:
+			process.stop()
+
 		pass
 
-	def restart(self, parts):
+	def restart(self, prog):
 		"sûrment moyen de faire self.stop et self.start l'un après l'autre"
-		print("restart de", parts)
+		print("restart de", prog)
+		self.stop(prog)
+		self.start(prog)
 		pass
 
 	def run_shell(self):
@@ -150,11 +240,11 @@ class Taskmaster:
 				case "reload":
 					self.reload()
 				case "start":
-					self.start(parts[1:])
+					self.start(parts[1])
 				case "stop":
-					self.stop(parts[1:])
+					self.stop(parts[1])
 				case "restart":
-					self.restart(parts[1:])
+					self.restart(parts[1])
 				case "exit":
 					self.running = False
 				case _:
